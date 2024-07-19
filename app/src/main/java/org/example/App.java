0b15e0fa0ab3d2c1658e1b91dc142e6496ae3e63
susd <susdomesticus@tutamail.com>
2024-07-19 13:08:17 +0300

    use AstPrinter in main

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -12,14 +12,20 @@ public class App {
     static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
-        }
+        Expr onePlusTwoGroup = new Expr.Grouping(
+                new Expr.Binary(new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)));
+        Expr fourMinusThreeGroup = new Expr.Grouping(
+                new Expr.Binary(new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)));
+        Expr expression = new Expr.Binary(
+                onePlusTwoGroup,
+                new Token(TokenType.STAR, "*", null, 1),
+                fourMinusThreeGroup);
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     private static void runFile(String path) throws IOException {
