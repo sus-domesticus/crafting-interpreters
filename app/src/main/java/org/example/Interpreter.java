0ea75a41e3590419ccc2d3a563069e5c43e2ad2f
susd <susdomesticus@tutamail.com>
2024-08-01 09:17:52 +0300

    feat(Chapter 8): add print and expression statements

--- app/src/main/java/org/example/Interpreter.java
+++ app/src/main/java/org/example/Interpreter.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.util.List;
+
 import org.example.Expr.Ternary;
 
-class Interpreter implements Expr.Visitor<Object> {
-    void interpret(Expr expression) {
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             App.runtimeError(error);
         }
@@ -24,6 +27,23 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 
     @Override
