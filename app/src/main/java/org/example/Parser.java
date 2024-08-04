fc31903555d497fc5aebbc4fe83d7f66df47611f
susd <susdomesticus@tutamail.com>
2024-08-04 16:10:19 +0300

    update TODO

--- app/src/main/java/org/example/Parser.java
+++ app/src/main/java/org/example/Parser.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// TODO: do the challenges [here](https://craftinginterpreters.com/classes.html).
+// TODO: continue [here](https://craftinginterpreters.com/inheritance.html).
 class Parser {
     private static class ParseError extends RuntimeException {
     }
